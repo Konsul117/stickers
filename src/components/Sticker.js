79a21c54a3d70cd7ff0b6c64c7d6e29e3a98b3a5
Konsul117 <konsul117@bk.ru>
2019-02-03 23:30:49 +1000

    Добавление, удаление стикеров - удаление drag'n'drop для мобильных экранов

--- src/components/Sticker.js
+++ src/components/Sticker.js
@@ -7,6 +7,15 @@ import * as React from "react";
  * @property {function}     props.onSetCoords Событие установки координат стикера. Передаёт аргументы: id, Coords
  * @property {function}     props.onMoved     Событие перемещения стикера. Передаёт аргументы: id, Coords
  * @property {function}     props.onClick     Событие клика по стикеру. Передаётся аргументом модель стикера
+ *
+ * @property {boolean}     state.isDragging      Идёт перемещение стикера через события onDragStart, onDragEnd
+ * @property {boolean}     state.isTouchDragging Идёт перемещение стикера через события для тач-экрана
+ * @property {number|null} state.x               Координата x начала перемещения для тач-экрана
+ * @property {number|null} state.y               Координата y начала перемещения для тач-экрана
+ * @property {number|null} state.stickerX        Координата x относительно границ стикера в начале перемещения для тач-экрана
+ * @property {number|null} state.stickerY        Координата y относительно границ стикера в начале перемещения для тач-экрана
+ * @property {number|null} state.width           Ширина стикера (для корректного отображения стикера во время перемещения)
+ * @property {number|null} state.height          Высота стикера (для корректного отображения стикера во время перемещения)
  */
 export default class Sticker extends React.PureComponent {
 	/**
@@ -16,13 +25,24 @@ export default class Sticker extends React.PureComponent {
 		super(props);
 
 		this.state = {
-			isDragging: false,
+			isDragging:      false,
+			isTouchDragging: false,
+			x:               null,
+			y:               null,
+			stickerX:        null,
+			stickerY:        null,
+			width:           null,
+			height:          null,
 		};
 
 		this.onDragStart    = this.onDragStart.bind(this);
 		this.onDragEnd      = this.onDragEnd.bind(this);
 		this.setRef         = this.setRef.bind(this);
 		this.onClick        = this.onClick.bind(this);
+
+		this.onTouchStart  =this.onTouchStart.bind(this);
+		this.onTouchEnd  =this.onTouchEnd.bind(this);
+		this.onTouchMove  =this.onTouchMove.bind(this);
 
 		this.stickerRef = null;
 	}
@@ -45,6 +65,7 @@ export default class Sticker extends React.PureComponent {
 		this.setState({
 			isDragging: false,
 		});
+
 		this.props.onMoved(this.props.sticker.id, {
 			beginX: e.clientX,
 			beginY: e.clientY,
@@ -77,6 +98,51 @@ export default class Sticker extends React.PureComponent {
 	}
 
 	/**
+	 * Обработка события касания к стикеру на тач-экране.
+	 *
+	 * @property {SyntheticEvent} e Событие
+	 */
+	onTouchStart(e) {
+		this.setState({
+			isTouchDragging: true,
+			width:           e.target.clientWidth,
+			height:          e.target.clientHeight,
+			x:               e.touches[0].pageX,
+			y:               e.touches[0].pageY,
+			stickerX:        e.touches[0].pageX-e.target.offsetLeft,
+			stickerY:        e.touches[0].pageY-e.target.offsetTop,
+		});
+	}
+
+	/**
+	 * Обработка события завершения перемещения при касании для тач-экрана.
+	 */
+	onTouchEnd() {
+		this.setState({
+			isTouchDragging: false,
+		});
+
+		this.props.onMoved(this.props.sticker.id, {
+			beginX: this.state.x,
+			beginY: this.state.y,
+			endX:   this.state.x + this.stickerRef.offsetWidth,
+			endY:   this.state.y + this.stickerRef.offsetHeight,
+		});
+	}
+
+	/**
+	 * Обработка события перемещения после касания на тач-экране
+	 *
+	 * @property {SyntheticEvent} e Событие
+	 */
+	onTouchMove(e) {
+		this.setState({
+			x: e.touches[0].pageX,
+			y: e.touches[0].pageY,
+		})
+	}
+
+	/**
 	 * Обработка клика по стикеру.
 	 */
 	onClick() {
@@ -87,21 +153,45 @@ export default class Sticker extends React.PureComponent {
 	 * @inheritdoc
 	 */
 	render() {
-		let divClass = 'sticker';
+		let wrapperClass = 'sticker';
 
-		if (this.state.isDragging) {
-			divClass += ' is-dragging';
+		let stickerStyle = {};
+		let wrapperStyle = {};
+
+		if (this.state.isTouchDragging || this.state.isDragging) {
+			wrapperClass += ' is-dragging';
 		}
 
-		return <div
-			ref={this.setRef}
-			className={divClass}
-			draggable="true"
-			onDragStart={this.onDragStart}
-			onDragEnd={this.onDragEnd}
-			onClick={this.onClick}
-		>
-			{this.props.sticker.text}
-		</div>
+		if (this.state.isTouchDragging) {
+			stickerStyle = {
+				position: 'absolute',
+				left:     this.state.x - this.state.stickerX,
+				top:      this.state.y - this.state.stickerY,
+				width:    this.state.width - 24,
+				height:   this.state.height - 24,
+			};
+
+			wrapperStyle = {
+				// width:  this.state.width,
+				height: this.state.height,
+			};
+		}
+
+		return <div className="sticker-wrapper" style={wrapperStyle}>
+			<div
+				ref={this.setRef}
+				className={wrapperClass}
+				draggable="true"
+				onDragStart={this.onDragStart}
+				onDragEnd={this.onDragEnd}
+				onTouchStart={this.onTouchStart}
+				onTouchEnd={this.onTouchEnd}
+				onTouchMove={this.onTouchMove}
+				onClick={this.onClick}
+				style={stickerStyle}
+			>
+				{this.props.sticker.text}
+			</div>
+		</div>;
 	}
 }
